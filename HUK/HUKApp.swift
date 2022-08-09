9bf584e69b0fe17602daad0396a4b546f03d307d
Eli Valine <elijahvaline@gmail.com>
2022-08-09 18:41:58 -0500

    first commit

---
+++ HUK/HUKApp.swift
@@ -0,0 +1,17 @@
+//
+//  HUKApp.swift
+//  HUK
+//
+//  Created by Elijah Valine on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HUKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to HUK (Heads Up Karaoke)
