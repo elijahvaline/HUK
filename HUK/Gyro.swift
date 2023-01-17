dab54fd5cdf272edeb3547f85d5408f6299ba94e
Eli Valine <elijahvaline@gmail.com>
2023-01-17 09:37:35 -0600

    fixed gyro

--- HUK/Gyro.swift
+++ HUK/Gyro.swift
@@ -9,38 +9,43 @@ import SwiftUI
 import CoreMotion
 
 let motion = CMMotionManager()
-var timer = Timer?
+//var timer = Timer?
 
 func startGyros() {
-   if motion.isGyroAvailable {
-      motion.gyroUpdateInterval = 1.0 / 60.0
-      motion.startGyroUpdates()
+//   if motion.isGyroAvailable {
+//      motion.gyroUpdateInterval = 1.0 / 60.0
+//      motion.startGyroUpdates()
 
       // Configure a timer to fetch the accelerometer data.
-      timer = Timer(fire: Date(), interval: (1.0/60.0),
-             repeats: true, block: { (timer) in
-         if let data = motion.gyroData {
-            let x = data.rotationRate.x
-            let y = data.rotationRate.y
-            let z = data.rotationRate.z
-
-            // Use the gyroscope data in your app.
-         }
-      })
+//      timer = Timer(fire: Date(), interval: (1.0/60.0),
+//             repeats: true, block: { (timer) in
+//         if let data = motion.gyroData {
+//            print(data.rotationRate.x)
+//            let x = data.rotationRate.x
+//            let y = data.rotationRate.y
+//            let z = data.rotationRate.z
+//
+//            // Use the gyroscope data in your app.
+//         }
+        motion.startGyroUpdates()
+       print(motion.gyroData?.rotationRate.x)
+       
+       
+//      })
 
       // Add the timer to the current run loop.
-      RunLoop.current.add(timer!, forMode: .defaultRunLoopMode)
-   }
+//      RunLoop.current.add(timer!, forMode: .defaultRunLoopMode)
+//   }
 }
 
 
-func stopGyros() {
-   if timer != nil {
-      timer?.invalidate()
-      timer = nil
-      motion.stopGyroUpdates()
-   }
-}
+//func stopGyros() {
+//   if timer != nil {
+//      timer?.invalidate()
+//      timer = nil
+//      motion.stopGyroUpdates()
+//   }
+//}
 
 struct GyroView: View {
     var body: some View {
